b103ef3e6cda758c1b137284e8695d3c36157960
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-13 12:06:07 -0500

    v1

--- ch1_09.py
+++ ch1_09.py
@@ -1,3 +1,5 @@
-# Name
-# Problem #
+# Name Who is going to St.Ives
+# Problem #10
 # Chapter 1 Problem Set Part 1
+
+print("As I was going to St.Ives I met a man with seven wives and each wife had seven sacks and each sack had seven cat and each cat had seven kittens so  how many are going to St.Ives 'for this problem assume that everything and everyone is going to St.Ives'")
