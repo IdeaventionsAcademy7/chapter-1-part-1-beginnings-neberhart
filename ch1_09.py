6a6bbc2ae6d70d5b6b7427af8e9327cf15c004f9
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-17 15:27:18 -0500

    You give it a number and then after doing math it gives the same number back
    Blah Blah Blah

--- ch1_09.py
+++ ch1_09.py
@@ -1,5 +1,18 @@
-# Name Who is going to St.Ives
-# Problem #10
+# Name Number Giver
+# Problem #9
 # Chapter 1 Problem Set Part 1
 
-print("As I was going to St.Ives I met a man with seven wives and each wife had seven sacks and each sack had seven cat and each cat had seven kittens so  how many are going to St.Ives 'for this problem assume that everything and everyone is going to St.Ives'")
+import math
+
+number_str = input("Input number: ")
+number_int = int(number_str)
+
+number = number_int
+
+number =(number + 2)* 3
+number = (number - 6)/3
+
+print("Your number is:",number, ", is this your original number?")
+
+
+ 
