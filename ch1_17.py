f482a18ee41320c08ad4c5b2ec2d18f464e7f947
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-20 11:33:42 -0500

    Integers, Strings, and Float Points

--- ch1_17.py
+++ ch1_17.py
@@ -1,3 +1,12 @@
-# Name
-# Problem #
+# Name integer, string, float
+# Problem #11
 # Chapter 1 Problem Set Part 1
+
+number = input("Put a number here: ")
+print(number)
+number = int(number)
+print(number)
+number = float(number)
+print(number)
+
+# You can input whole number without any errors appearing anything else will cause a syntax error
