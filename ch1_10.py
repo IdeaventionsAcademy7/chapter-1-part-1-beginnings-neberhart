0dee21cf87927613e9c3fbc8a04cb821bd5ff5f8
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-20 11:17:48 -0500

    St.Ives Nursery Rhyme
    Solves a nursery rhyme

--- ch1_10.py
+++ ch1_10.py
@@ -1,3 +1,32 @@
-# Name
-# Problem #
+# Name I can't believe I'm solving a nursery Ryhme
+# Problem #10
 # Chapter 1 Problem Set Part 1
+
+print("As I was going to St. Ives I met a man with seven wives each wife had seven, "\
+      " , sacks and in each sack there were seven cats and each cat had seven kittens " \
+      "how many were going  St. Ives")
+      
+
+
+
+W = 1
+
+S = 1 
+
+C = 1
+
+K = 1
+
+W = W * 7
+
+S = (W * 7)
+
+C = (S * 7)
+
+K = (C * 7)
+
+number = 2 + K + C + S + W
+
+print("The number of people going to St. Ives is", number ,)
+
+
