f4ea8a64964b1bfcdb97cc45fb4a7145ee785ba1
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-20 16:31:42 -0500

    Tells you how deep it would be if you poured all the water of the great lakes on the united states

--- ch1_Project_GreatLakes.py
+++ ch1_Project_GreatLakes.py
@@ -1,3 +1,8 @@
-# Name
+# Name Great Lakes are big
 # Project: Great Lakes are how big?
 # Chapter 1 Problem Set Part 1
+
+print("How deep would it be if all the water in the Great Lakes was spread evenly across the contigous United States.")
+
+depth = 22810/8080464.3
+print("It would be this deep:",depth*1000, " meters")
